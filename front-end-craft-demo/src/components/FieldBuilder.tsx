f281ca1812fbee209cdcafd37945b46b40af85ae
Paul Rocheteau <rocheteau.p@gmail.com>
2021-10-05 23:04:49 -0400

    revised form styling and color scheme, changed browser tab name

--- front-end-craft-demo/src/components/FieldBuilder.tsx
+++ front-end-craft-demo/src/components/FieldBuilder.tsx
@@ -6,18 +6,35 @@ function FieldBuilder() {
         <div className='form'>
             <h3>Field Builder</h3>
             <form>
-                <label htmlFor="label">Label</label>
-                <input type="text" id="label" name="label"/>
-                <label htmlFor="type">Type</label>
-                <input type="text" id="type" name="type"/>
-                <label htmlFor="defaultValue">Default Value</label>
-                <input type="text" id="defaultValue" name="defaultValue"/>
-                <label htmlFor="choices">Choices</label>
-                <input type="text" id="choices" name="choices"/>
-                <label htmlFor="order">Order</label>
-                <input type="text" id="order" name="order"/>
-                <input type="submit" value="Submit"/>
-                <input type="button" value="Cancel"/>
+                <fieldset>
+                    <div className="formGroup">
+                        <label htmlFor="label">Label</label>
+                        <input type="text" id="label" name="label"/>
+                    </div>
+
+                    <div className="formGroup">
+                        <label htmlFor="type">Type</label>
+                        <input type="text" id="type" name="type"/>
+                    </div>
+
+                    <div className="formGroup">
+                        <label htmlFor="defaultValue">Default Value</label>
+                        <input type="text" id="defaultValue" name="defaultValue"/>
+                    </div>
+
+                    <div className="formGroup">
+                        <label htmlFor="choices">Choices</label>
+                        <input type="text" id="choices" name="choices"/>
+                    </div>
+
+                    <div className="formGroup">
+                        <label htmlFor="order">Order</label>
+                        <input type="text" id="order" name="order"/>
+                    </div>
+
+                    <input type="submit" value="Submit"/><span>Or</span>
+                    <input type="button" value="Cancel"/>
+                </fieldset>
             </form>
         </div>
     );
