27c04d13f111b9eb63e614ba0bc98b21fb92df49
Paul Rocheteau <rocheteau.p@gmail.com>
2021-10-07 22:07:50 -0400

    added more responsive form styling, added select input fields, added clear toast message

--- front-end-craft-demo/src/components/FieldBuilder.tsx
+++ front-end-craft-demo/src/components/FieldBuilder.tsx
@@ -3,43 +3,60 @@ import '../index.css';
 import toast, { Toaster } from 'react-hot-toast';
 
 function FieldBuilder() {
-    const notify = () => toast.success('Fields have been saved!');
+    const notifySubmit = () => toast.success('Fields have been saved!');
+    const notifyClear = () => toast.success('Fields have been reset!');
 
     return (
-        <div className='form'>
+        <div className="fieldBuilder">
             <h3>Field Builder</h3>
-            <form>
-                <fieldset>
-                    <div className="formGroup">
-                        <label htmlFor="label">Label</label>
-                        <input type="text" id="label" name="label"/>
-                    </div>
+            <div className='form'>
+                <form>
+                    <fieldset>
+                        <div className="formGroup">
+                            <label htmlFor="label">Label</label>
+                            <input type="text" id="label" name="label"/>
+                        </div>
 
-                    <div className="formGroup">
-                        <label htmlFor="type">Type</label>
-                        <input type="text" id="type" name="type"/>
-                    </div>
+                        <div className="formGroup">
+                            <label htmlFor="type">Type</label>
+                            <input type="text" id="type" name="type"/>
+                        </div>
 
-                    <div className="formGroup">
-                        <label htmlFor="defaultValue">Default Value</label>
-                        <input type="text" id="defaultValue" name="defaultValue"/>
-                    </div>
+                        <div className="formGroup">
+                            <label htmlFor="defaultValue">Default Value</label>
+                            <input type="text" id="defaultValue" name="defaultValue"/>
+                        </div>
 
-                    <div className="formGroup">
-                        <label htmlFor="choices">Choices</label>
-                        <input type="text" id="choices" name="choices"/>
-                    </div>
+                        <div className="formGroup choices">
+                            <label htmlFor="choices">Choices</label>
+                            <select name="choices" id="choices" multiple>
+                                <option value="asia">Asia</option>
+                                <option value="australia">Australia</option>
+                                <option value="westernEurope">Western Europe</option>
+                                <option value="northAmerica">North America</option>
+                                <option value="easternEurope">Eastern Europe</option>
+                                <option value="latinAmerica">Latin America</option>
+                                <option value="middleEast-africa">Middle East and Africa</option>
+                            </select>
+                        </div>
 
-                    <div className="formGroup">
-                        <label htmlFor="order">Order</label>
-                        <input type="text" id="order" name="order"/>
-                    </div>
+                        <div className="formGroup">
+                            <label htmlFor="order">Order</label>
+                            <select name="order" id="order">
+                                <option value="alphabetical">Display choices in Alphabetical</option>
+                                <option value="reverse">Display choices in Reverse Alphabetical</option>
+                            </select>
+                        </div>
 
-                    <input onClick={notify} type="button" value="Submit"/><span>Or</span>
-                    <input type="button" value="Cancel"/>
-                    <Toaster />
-                </fieldset>
-            </form>
+                        <div className="button-row">
+                            <input className="submit-button" onClick={notifySubmit} type="button" value="Submit"/>
+                            <input onClick={notifyClear} type="button" value="Clear"/>
+                            <input type="button" value="Cancel"/>
+                        </div>
+                        <Toaster />
+                    </fieldset>
+                </form>
+            </div>
         </div>
     );
 }
